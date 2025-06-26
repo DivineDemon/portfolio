ef0c62a8d72535bb7b82ab6b62518fcec65db71e
DivineDemon <supame123@gmail.com>
2025-06-26 16:05:41 +0500

    Vercel Analytics.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from "next";
 import { Calistoga, Inter } from "next/font/google";
 
+import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "@/assets/css/globals.css";
@@ -61,6 +62,7 @@ export default function RootLayout({
           calistoga.variable
         )}
       >
+        <Analytics />
         <SpeedInsights />
         <TRPCReactProvider>{children}</TRPCReactProvider>
       </body>
